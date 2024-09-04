dd188fffd3f41106586842d8821bf4f1c84fcb15
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2024-09-04 11:41:30 +0600

    Add files via upload

---
+++ set_12.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    set<string, greater<string>> s;
+
+    s.insert("omaer");
+    s.insert("md");
+    s.insert("omaer");
+    s.insert("alam");
+    s.insert("md");
+    s.insert("alam");
+
+    cout<<s.size()<<endl;
+    for(auto u:s) cout<<u<<endl;
+
+return 0;
+}
+
+
