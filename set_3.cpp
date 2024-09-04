266bc459a44ec3e816cbeb5f4f002b292ff23994
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2024-09-04 11:38:27 +0600

    Add files via upload

---
+++ set_3.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    set<int> s={1,2,3,1,4,1,2,4,5,245,63,245,245,2,125,234};
+
+    s.clear();
+    cout<<s.empty()<<endl;
+
+return 0;
+}
+
