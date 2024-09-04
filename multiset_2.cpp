145406c7029da63987b5e7f2ddb4072d14fe0ebf
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2024-09-04 11:42:43 +0600

    Add files via upload

---
+++ multiset_2.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    multiset<int> s;
+
+    s.insert(1);//log2(n)
+    s.insert(3);
+    s.insert(1);
+    s.insert(4);
+    s.insert(1);
+
+    s.erase(1);
+    for(auto u:s) cout<<u<<" ";
+    cout<<endl;
+
+return 0;
+}
+
