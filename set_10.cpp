8223b0ac6737bf08fe2d4fb758cef8ac17245310
OmaerAlam <114277920+OmaerAlam@users.noreply.github.com>
2024-09-04 11:41:01 +0600

    Add files via upload

---
+++ set_10.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    set<pair<int,int>> s;
+
+    s.insert({2,3});
+    s.insert({4,1});
+    s.insert({2,3});
+    s.insert({4,1});
+    s.insert({5,1});
+    s.insert({1,3});
+
+    cout<<s.size()<<endl;
+    for(auto u:s) cout<<u.first<<" "<<u.second<<endl;
+
+return 0;
+}
+
+
